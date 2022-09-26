742f2cabce25d3fa69dcaab28c5d99eaed5e1221
Jmart5564 <Jmart5564@gmail.com>
2022-09-26 14:49:17 -0700

    added code to Book Detail, books are not yet clickable to show detail

--- src/components/book/BookDetail.js
+++ src/components/book/BookDetail.js
@@ -1,6 +1,6 @@
 import Book from './Book';
 import { useBookDetail } from '../../hooks/useBookDetail';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { useEffect } from 'react';
 import { getBookById } from '../../services/books';
 
@@ -15,7 +15,8 @@ function BookDetail() {
   if (error)
     return (
       <>
-        <h1>Something went wrong :(</h1> <h2>{error}</h2>
+        <Book book={book} showDetail />
+        <Link to="/books">Back to Catalog</Link>
       </>
     );
 
